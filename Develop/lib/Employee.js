8ab762c23cc7e40242d54a6175b4a8f45d26b90a
Rob Kellen <rob.h.kellen@gmail.com>
2020-08-31 09:26:28 -0700

    created Employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+  // Just like constructor functions, classes can accept arguments
+  constructor(name, role, id, officeNumber) {
+    this.name = name;
+    this.role = role;
+    this.id = id;
+    this.officeNumber = officeNumber;
+  }
+
+  printInfo() {
+    console.log(`Name: ${this.name}`);
+    console.log(`Role: ${this.role}`);
+    console.log(`ID: ${this.id}`);
+    console.log(`Office Number: ${this.officeNumber}`);
+  }
+}
+
+const employee = new Employee("Rob", "Manager", 1, 1);
+
+employee.printInfo();
